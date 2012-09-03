a4ff7e444465e59ad7b37230f5bec13e4309ba19
Junior Rimac <junior.rimac@gmail.com>
2012-09-03 13:38:40 -0500

    First commit...

---
+++ src/com/juniorsrr/web/LoginValidator.java
@@ -0,0 +1,29 @@
+package com.juniorsrr.web;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.ValidationUtils;
+
+import com.juniorsrr.web.*;
+
+public class LoginValidator implements Validator {
+	@Override
+	public boolean supports(Class clazz) {
+		return Login.class.isAssignableFrom(clazz);
+	}
+	
+	public void validate(Object obj, Errors errors){
+		Login login = (Login) obj;
+		if( login.getUsername() == null || login.getUsername().length() == 0 ){
+			errors.rejectValue( "username", "error.empty.field", "Please Enter User Name" );
+		}else if( !login.getUsername().equals("admin") ){
+			errors.rejectValue( "username", "unknown.user", "Unknown User" );
+		}
+		if( login.getPassword() == null || login.getPassword().length() == 0 ){
+			errors.rejectValue( "password", "error.empty.field", "Please Enter Password" );
+		}else if( !login.getPassword().equals("admin") ){
+			errors.rejectValue( "password", "wrong.passsword", "Wrong Password" );
+		}
+	}
+	
+}
